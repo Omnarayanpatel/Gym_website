8a36f846bce047ac929e4261b2484b3c15aed542
Om Narayan Patel <omnarayan_p@ad.avyaanmgmt.com>
2026-05-16 16:04:12 +0530

    testing completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import MainLayout from './layouts/MainLayout';
 import AppRoutes from './routes/AppRoutes';
+import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
     <MainLayout>
+      <ScrollToTop />
       <AppRoutes />
     </MainLayout>
   );
